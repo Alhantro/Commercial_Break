c59f26cbb253e016734fee33d588a0d40b1f9063
Kevin Mettes <k.mettes@home.nl>
2013-10-28 11:57:10 +0100

    resync
    fix syncing problem hopefully

--- Graybox/test_Data/Scripts/City/HUD/Constants.js
+++
@@ -1,4 +0,0 @@
-#pragma strict
-
-static var HUD_X = 0;								//top left corner of screen	
-static var HUD_Y = Screen.height * 0.80;			//90% of screen height
--- Graybox/test_Data/Scripts/JobSystem/Selection.js
+++
@@ -1,9 +0,0 @@
-#pragma strict
-
-public var proceedBoolean:boolean;
-
-function OnMouseUp()
-{
-	if(proceedBoolean) Debug.Log("correct! yes!");
-	else Debug.Log("wrong one!");
-}
